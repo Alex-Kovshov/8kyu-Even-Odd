bcfd5045d5ba70bda86a515615099e3b11ee6d3d
Aleksandr <koshov198298@gmail.com>
2023-01-20 14:09:31 +0900

    8kyu EvenOdd

---
+++ main.js
@@ -0,0 +1,17 @@
+/* Create a function that takes
+integer as argument and returns
+ "Even" for even numbers, or
+"Odd" for odd numbers. */
+
+function evenOrOdd(number) {
+  if(number % 2==0){
+   return "Even"
+  } else  {
+    return 'Odd'
+  }
+}
+
+function evenOrOdd(number) {
+  if(number % 2==0) return "Even"
+   return 'Odd'
+}
